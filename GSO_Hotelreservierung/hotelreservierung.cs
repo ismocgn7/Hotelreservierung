982c0820ac0f6edba968ab7e2ec96cfeff74af85
Ismael Zanati <ismael.z2@gso.schule.koeln>
2024-03-28 00:56:03 +0100

    Update Program.cs

---
+++ GSO_Hotelreservierung/hotelreservierung.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GSO_Hotelreservierung
+{
+    internal class hotelreservierung
+    {
+        public void AppStart()
+        {
+            Console.WriteLine("Ich kann nicht Programmieren!");
+        }
+    }
+}
